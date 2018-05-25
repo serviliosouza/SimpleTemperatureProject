c49c8e843136968971663262f10135df8bafbe34
Servílio <servilio.souza@gmail.com>
2018-05-25 14:44:39 -0300

    db update

--- DATABASE/setup-database.sql
+++ DATABASE/setup-database.sql
@@ -12,6 +12,7 @@ create table tempdata (
 	id           integer(8)  not null auto_increment,
 	sensor_id    varchar(30)  not null,
 	temperature  integer(8)  not null,
+	dt           timestamp default current_timestamp not null,
 	
 	primary key (id)
 );
